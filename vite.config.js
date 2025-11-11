349c6b85092c72c1793a7ada23b5cf7b5018d767
Tejas1024 <tejaspavithra2002@gmail.com>
2025-11-11 15:16:08 +0530

    added proper files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+javascript
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Use '/' if your repo is username.github.io, otherwise '/repo-name/'
 })
